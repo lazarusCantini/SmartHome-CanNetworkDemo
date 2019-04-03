a7e2326a72672231a1cc468b06836afddbfbe6b6
Bastian-Reul <45174161+Bastian-Reul@users.noreply.github.com>
2019-04-03 15:06:09 +0200

    gitignore Test

---
+++ Node_01_Keller_1_Bastelkeller/Node_01_Keller_1_Bastelkeller/Node_Muster/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Hausbus.cpp
+
+Sketch.cpp
+
+src\libraries\CAN\src\CANController.cpp
+
+src\libraries\CAN\src\ESP32SJA1000.cpp
+
+src\libraries\CAN\src\MCP2515.cpp
+
+src\libraries\SPI\src\SPI.cpp
+
